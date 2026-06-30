7f454d5988e730921ece2fbe13548432ee02eda3
kunal-anand07 <kunalanand7325@gmail.com>
2026-06-30 21:42:18 +0530

    Added URL History table with Analytics

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const URL = require("../models/url");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    return res.render("Home");
+router.get("/", async (req, res) => {
+    const allurls = await URL.find({});
+    return res.render("Home",{
+        urls : allurls,
+    });
 });
 module.exports = router;
